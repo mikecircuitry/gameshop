0b0198d65d1ff3bbe9608d19c082e330a8e85030
mike <mikecircuitry@gmail.com>
2025-05-19 09:44:10 -0400

    Add quantity adjustment buttons to product item component

--- src/app/cart/components/productItem.tsx
+++ src/app/cart/components/productItem.tsx
@@ -13,13 +13,29 @@ const item = prop.product;
             <div className="card-body">
                 <div className="row">
                     <div className="col-md-2">
-                        <img src={item.image} className="rounded float-start card-img-top"  />
+                        <img src={item.image} className="rounded float-start card-img-top" alt={item.name}  />
                     </div>
                     <div className="col-md-4">
                         <h5 className="card-title text-centerx" style={{paddingTop:"7%"}}>{item.name}</h5>
                     </div>
                     <div className="col-sm-4">
-                        <h5 className="card-title text-centerx" style={{paddingTop:"7%"}}>{item.quantity}</h5>
+                        <div className="input-group mb-3">
+                            <button className="input-groupx btn btn-light col-sm-1">
+                                <span>  - </span>
+                            </button>
+                            <input type="text" className="col-sm-2 text-center" style={{backgroundColor: "#fff", color: "#000"}} id={item.name}
+                                   value={item.quantity}/>
+                            <button className="input-groupx btn btn-light col-sm-1">
+                                <span>  + </span>
+                            </button>
+                            {/*<span className="input-group-text" id="basic-addon1">@</span>*/}
+                            {/*<input type="text" className="form-control" placeholder="Username" aria-label="Username"*/}
+                            {/*       aria-describedby="basic-addon1"/>*/}
+                        </div>
+
+
+
+                        {/*<h5 className="card-title text-centerx" style={{paddingTop: "7%"}}>{item.quantity}</h5>*/}
                     </div>
                 </div>
 
