1c1febcaabeeebd14fb75ccc22ce4991e059717c
mike <mikecircuitry@gmail.com>
2025-06-28 17:46:48 -0400

    Replace Bootstrap with TailwindCSS and add related dependencies
    started working on adding tailwind css styles

--- src/components/AddBootstrap.tsx
+++ src/components/AddBootstrap.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function AddBootstrap() {
   useEffect(() => {
-      import("bootstrap");
+     // import("bootstrap");
 
   }, []);
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,42 @@
-'use client'
+"use client";
 import Link from "next/link";
-import {useCart} from "@/contexts/cartContext";
+import { useCart } from "@/contexts/cartContext";
+import Image from "next/image";
 
 export default function NavBar() {
-    const { cartItemCount } = useCart();
+  const { cartItemCount } = useCart();
 
-    return (
-        <nav className="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
-            <div className="container-fluid">
-                <Link className="navbar-brand" href="/">
-                    <img src="https://gmedia.playstation.com/is/image/SIEPDC/dualsense-controller-product-thumbnail-01-en-14sep21?$facebook$" alt="Logo" width="30" height="24"
-                         className="d-inline-block align-text-top"/>
-                    My Game Shop
-                </Link>
-                <Link className="navbar-brand text-white" href="/cart">
-                    {/*<img src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png" alt="Cart" width="30" height="24"*/}
-                    {/*     className="d-inline-block align-text-top"/>*/}
-                    <i className="bi bi-cart text-white"></i>
-                    Cart
-                    {cartItemCount > 0 && (
-                        <span
-                            className="position-absolutex top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                            {cartItemCount}
-                        </span>
-                    )}
-                </Link>
+  return (
+    <nav className="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="/">
+          <img
+            src="https://gmedia.playstation.com/is/image/SIEPDC/dualsense-controller-product-thumbnail-01-en-14sep21?$facebook$"
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          My Game Shop
+        </Link>
+        <Link className="navbar-brand text-white" href="/cart">
+          {/*<img src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png" alt="Cart" width="30" height="24"*/}
+          {/*     className="d-inline-block align-text-top"/>*/}
+          <i className="bi bi-cart text-white"></i>
+          Cart
+          {cartItemCount > 0 && (
+            <span className="position-absolutex translate-middle badge rounded-pill bg-danger start-100 top-0">
+              {cartItemCount}
+            </span>
+          )}
+        </Link>
 
-                {/*<Link className="navbar-brand" href="/game">*/}
-                {/*    <img src="https://gmedia.playstation.com/is/image/SIEPDC/dualsense-controller-product-thumbnail-01-en-14sep21?$facebook$" alt="Logo" width="30" height="24"*/}
-                {/*         className="d-inline-block align-text-top"/>*/}
-                {/*    My Game Shop*/}
-                {/*</Link>*/}
-            </div>
-        </nav>
-
-    );
+        {/*<Link className="navbar-brand" href="/game">*/}
+        {/*    <img src="https://gmedia.playstation.com/is/image/SIEPDC/dualsense-controller-product-thumbnail-01-en-14sep21?$facebook$" alt="Logo" width="30" height="24"*/}
+        {/*         className="d-inline-block align-text-top"/>*/}
+        {/*    My Game Shop*/}
+        {/*</Link>*/}
+      </div>
+    </nav>
+  );
 }
